1466bfd06c00d4ed157c8a04431aea81994d4fb4
Abdelsalam <Abdelsalam_PC@ABDELSALAM-PC>
2022-10-22 16:01:27 +0200

    added the id specification criteria

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -36,7 +36,8 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _productsRepo.GetByIdAsync(id);
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            return await _productsRepo.GetEntityWithSpecification(spec);
         }
 
         [HttpGet("brands")]
