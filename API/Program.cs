f8a5007b6bacbf4eafa3246c0dc1aaae2c3793ad
Abdelsalam <Abdelsalam_PC@ABDELSALAM-PC>
2022-11-16 21:23:18 +0200

    Initializing UI

--- API/Program.cs
+++ API/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:7151");
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
     });
 });
 
