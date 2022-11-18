9aeb505d63a42bd2be9452b5faa5c57f1bb7cac1
Abdelsalam <Abdelsalam_PC@ABDELSALAM-PC>
2022-11-18 19:40:02 +0200

    Added Lazy Loading to Routers & added Shop Router

---
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  product!: IProduct;
+  paramId: any = this.activatedRoute.snapshot.paramMap.get('id');
+
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    this.shopService.getProduct(this.paramId as number).subscribe(product => { this.product = product }, error => { console.log(error) })
+  }
+
+
+
+}
